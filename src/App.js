534efb499656ba12df7694669e810137bb6be2de
abelko <esung1129@gmail.com>
2019-11-10 14:21:48 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+window.location = "https://docs.google.com/forms/d/e/1FAIpQLScMKIOg3ZMsEUCzd7crtu0BVTUjAjtl2JobY4ylmxvIKshM8g/viewform";
+
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
